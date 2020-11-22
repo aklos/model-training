d8ca06f11db62c9d064a33b44f09d249c7567f1b
Alex Klos <alexklos@fastmail.com>
2020-11-22 19:55:37 +0000

    run with python3

--- train.sh
+++ train.sh
@@ -14,4 +14,4 @@ git clone https://github.com/aklos/stylegan2.git
 cd stylegan2/
 
 # Run model training
-python run_training.py --num-gpus=1 --data-dir=/storage/datasets --config=config-f --dataset=lowpoly --mirror-augment=true --metrics=None
+python3 run_training.py --num-gpus=1 --data-dir=/storage/datasets --config=config-f --dataset=lowpoly --mirror-augment=true --metrics=None
