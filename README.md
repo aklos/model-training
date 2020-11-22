703903ac84d85aa550ca85febefa4248c688e3d5
Alex Klos <alexklos@fastmail.com>
2020-11-22 20:46:53 +0000

    output training results to /storage/results

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Docker container: `tensorflow/tensorflow:1.14.0-py3`
+CPU container: `tensorflow/tensorflow:1.14.0-py3`
+GPU container: `tensorflow/tensorflow:1.14.0-gpu-py3`
--- train.sh
+++ train.sh
@@ -15,8 +15,11 @@ pip install -r requirements.txt
 rm -rf stylegan2/
 git clone https://github.com/aklos/stylegan2.git
 
+# Make result folder if it doesn't exist
+mkdir -p /storage/results
+
 # Go into stylegan2 repo
 cd stylegan2/
 
 # Run model training
-python3 run_training.py --num-gpus=1 --data-dir=/storage/datasets --config=config-f --dataset=lowpoly --mirror-augment=true --metrics=None
+python3 run_training.py --num-gpus=1 --data-dir=/storage/datasets --config=config-f --dataset=lowpoly --mirror-augment=true --metrics=None --result-dir=/storage/results
